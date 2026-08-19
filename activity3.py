06099b5504d8c510de2f5f8dd81c51b1a21b21a2
Tinchrist <christincmerle@gmail.com>
2026-08-19 19:29:01 +0800

    Add files via upload

---
+++ activity3.py
@@ -0,0 +1,37 @@
+#integer variable
+myAge = 18
+currentYear = 2026
+birthYear = 2008
+myCats = 5
+dayofbirth = 4
+
+#float variable
+myHeight = 1.47
+myWeight = 74.96
+
+
+#string variable
+firstname = "Christin"
+surname = "Merle"
+school = "PNHS"
+birthmonth = "April"
+
+#boolean variable
+isAlive = True
+isagirl = True
+likescats = True
+likessleeping = True
+
+print("Hi my name is", firstname, surname)
+print("and I am", myAge)
+print("When I'm doing this activity, it is in the year", currentYear)
+print("I graduated highschool at", school)
+print("I was born on", birthmonth, dayofbirth, birthYear)
+print("My current height right now is(in meters):", myHeight)
+print("and also my weight currently is(in pounds):", myWeight)
+print(myCats, "cats that are alive right now, the other ones died")
+print("Hi future me if you ever revisit this, I hope you are alive and well,", isAlive)
+print("We are a girl, obviously haha", isagirl)
+print("We like cats", likescats)
+print("We like to sleep", likessleeping)
+
