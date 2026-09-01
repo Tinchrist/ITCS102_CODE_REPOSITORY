2afd170762accc8b6747108df477d63613ee2241
Tinchrist <christincmerle@gmail.com>
2026-09-01 15:19:43 +0800

    Add files via upload

---
+++ activity5.py
@@ -0,0 +1,26 @@
+#arithmetic operators = mathematical symbols
+# + - addition
+# - - subtraction
+# * - multiplication
+# / - division 
+# // - floor division
+# ** - exponent
+# % - modulus
+
+a = 125
+b = 4
+sum = a + b
+diff = a - b
+product = a * b
+quotient = a / b
+floordivision  = a // b
+exponentation  = a ** b
+modulus  = a % b
+
+print('The sum of ',a,'plus',b,'is', sum)
+print('The difference of ',a,'minus',b,'is', diff)
+print('The product of ',a,'times',b,'is', product)
+print('The quotient of ',a,'and',b,'is', quotient)
+print('The floordivision of ',a,'and',b,'is', floordivision)
+print('The exponentation of ',a,'and',b,'is', exponentation)
+print('The modulus of ',a,'and',b,'is' , modulus)
